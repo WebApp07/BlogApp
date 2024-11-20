b11f1d57994dc7a5110648265624eb4322007c2f
unknown <aminezouguig@proton.me>
2024-11-20 16:55:28 +0100

    Responsive Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className="w- h-16 md:h-20 flex items-center justify-between">
+      <div className="flex items-center gap-4 text-2xl font-bold">
         {/* LOGO */}
         <img src="/logo.png" alt="logo" className="w-8 h-8" />
         <span>AmineBlog</span>
       </div>
       {/* MOBILE MENU */}
+      <div className="md:hidden">m</div>
       {/* DESKTOP MENU */}
+      <div className="hidden md:flex">D</div>
     </div>
   );
 };
