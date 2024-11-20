91deabe3c2b8b88ca7e3bb9423edf069eaf5d730
unknown <aminezouguig@proton.me>
2024-11-20 19:12:19 +0100

    Menu Desing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="w- h-16 md:h-20 flex items-center justify-between">
       <div className="flex items-center gap-4 text-2xl font-bold">
@@ -9,9 +11,44 @@ const Navbar = () => {
         <span>AmineBlog</span>
       </div>
       {/* MOBILE MENU */}
-      <div className="md:hidden">m</div>
+      <div className="md:hidden">
+        {/*MOBILE BUTTON */}
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "="}
+        </div>
+
+        {/*MOBILE LINK LIST */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login🙋
+            </button>
+          </a>
+        </div>
+      </div>
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login🙋
+          </button>
+        </a>
+      </div>
     </div>
   );
 };
