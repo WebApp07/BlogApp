35b49958a7d3893b62de5a20902cd6f3c6ab509d
unknown <aminezouguig@proton.me>
2024-11-20 16:50:29 +0100

    Responsive Layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        {/* LOGO */}
+        <img src="/logo.png" alt="logo" className="w-8 h-8" />
+        <span>AmineBlog</span>
+      </div>
+      {/* MOBILE MENU */}
+      {/* DESKTOP MENU */}
+    </div>
+  );
+};
+
+export default Navbar;
