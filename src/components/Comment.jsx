416844e26f2d930ac7e261323b2ac1ebaa2d1420
unknown <aminezouguig@proton.me>
2024-11-24 01:59:38 +0100

    Comments Section Design

---
+++ src/components/Comment.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "./Image";
+
+const Comment = () => {
+  return (
+    <div className="p-4 bg-slate-50 rounded-xl mb-8">
+      <div className="flex items-center gap-4">
+        <Image
+          src="userImg.jpeg"
+          className="w-10 h-10 rounded-full object-cover"
+          w="40"
+        />
+        <span className="font-medium">Jogn Doe</span>
+        <span className="text-sm text-gray-500">2 days ago</span>
+      </div>
+      <div className="mt-4">
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
